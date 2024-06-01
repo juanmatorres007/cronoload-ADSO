441d0ddd620dcb7f595f5fd7dad0f1a0f454456f
Samito200 <samesteban2005@gmail.com>
2024-05-31 19:48:08 -0500

    If Rol as

--- routes/ifRol.php
+++ routes/ifRol.php
@@ -1,3 +1,14 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
 
 include_once "../controller/ifRolController.php";
+
+$ifRolController = new ifRolController();
+
+$id_usuario = $_SESSION['usuario']['id_auto_user'];
+
+  $getSessionRol = $ifRolController->getSessionRol($id_usuario);
+  
+  return $getSessionRol;
