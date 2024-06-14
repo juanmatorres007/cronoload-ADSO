b3cbb3d9217e0673b43dc637df8be8a5558bdd13
juanmatorres007 <131231838+juanmatorres007@users.noreply.github.com>
2024-06-13 19:50:48 -0500

    Delete Config directory

--- Config/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-const base_url = "http://localhost/cronoload/";
-const host = "localhost";
-const user = "root";
-const pass = "";
-const db = "programador2";
-const charset = "charset=utf8";
-?>
